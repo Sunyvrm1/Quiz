0d2a282f327b49c79901a8b613a9f826b15b7449
Sushil Verma <115399549+Sunyvrm1@users.noreply.github.com>
2024-03-13 10:15:09 +0530

    file_uploaded

---
+++ script.js
@@ -0,0 +1,52 @@
+fetch("quiz.json")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data);
+
+    data.results.map((quiz, i) => {
+        const quizCont = document.querySelector(".quizCont");
+        quizCont.insertAdjacentHTML("beforebegin", 
+        `<p>Question: ${i+1}</p>
+        <form>
+      <label for="" id="${quiz.correct_answer}">${quiz.question}</label><br><br>
+      <input type="radio" class="mcq_${i}" name="question_${i}" value="${quiz.option1}"/>
+      <label>${quiz.option1}</label><br />
+      <input type="radio" class="mcq_${i}" name="question_${i}" value="${quiz.option2}"/>
+      <label>${quiz.option2}</label><br />
+      <input type="radio" class="mcq_${i}" name="question_${i}" value="${quiz.option3}"/>
+      <label>${quiz.option3}</label><br />
+      <input type="radio" class="mcq_${i}" name="question_${i}" value="${quiz.option4}"/>
+      <label>${quiz.option4}</label><br /><br />
+      <input type="submit" value="Submit">
+      <p class="result"></p>
+    </form>`)
+    })
+
+    const form = document.querySelectorAll("form");
+    form.forEach((form, i) => {
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        console.log(`Form no. ${i+1}`)
+        const selectedOption = form.querySelector(`input[name="question_${i}"]:checked`).value;
+        const correctAns = form.querySelector("label").id;
+        console.log(selectedOption)
+        console.log(correctAns);
+
+        const result = document.querySelectorAll(".result");
+        if(selectedOption === correctAns){
+          result[i].textContent = "Correct ✅"
+        } else{
+          result[i].textContent = "Wrong ❌"
+        }
+
+        const allInput = document.querySelectorAll(`.mcq_${i}`);
+        allInput.forEach((input) => {
+          input.addEventListener("click", () => {
+            result[i].style.display = "none";
+          })
+        })
+        result[i].style.display = "block";
+      })
+    })
+})
